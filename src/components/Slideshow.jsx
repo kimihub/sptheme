101ee70751982e3e4a2ecdc6906385724daa3009
kimi <kimhimitsu@gmail.com>
2016-12-24 02:13:26 +0100

    feat(slideshow): new slideshow system with images preloader

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -4,37 +4,56 @@ import { h, Component } from 'preact';
 
 export default class Slideshow extends Component {
 
-  constructor (props) {
-    super(props);
-
-    this.state = {
-      slides: [],
-    };
-  }
+  state = {
+    spinnerClassName: 'spinner',
+    slides: '',
+  };
 
   componentDidMount() {
     fetch('/datas/Slideshow.json')
       .then(res => {
         return res.json();
       })
-      .then(json => {
-        this.setState({slides: json._});
-      })
+      .then(this.preloadImages.bind(this))
       .catch(err => {
         console.log('error', err);
       });
-
   }
 
-  render () {
+  preloadImages(json) {
+    let count = 0;
+    json._.forEach((slide, i) => {
+      let img = new Image();
+      let url = '/medias/slideshow/' + slide;
+      let style = {backgroundImage: `url(${url})`};
+      img.onload = e => {
+        count++;
+
+        // last image loaded
+        if (count > i) {
+          this.setState({
+            slides: json._,
+            spinnerClassName: 'spinner hide',
+          })
+        }
+
+      };
+      img.src = url;
+      json._[i] = <div key={i} className="slide" style={style}></div>;
+    });
+  }
+
+  render ({}, state) {
     return (
     	<section className="slideshow">
-        <div className="mask">
-          <h1 className="slideshow-title"><span className="access-text">Studio Némésis</span></h1>
+        <div className={state.spinnerClassName}>
+          <div className="bounce1"></div>
+          <div className="bounce2"></div>
+          <div className="bounce3"></div>
         </div>
-        {this.state.slides.map((slide, i) => (
-          <div key={i} className="u-full-width slide" style={{backgroundImage: 'url(/medias/slideshow/' + slide + ')'}}></div>
-        ))}
+        {state.slides}
+        <div className="mask"></div>
+        <h1 className="slideshow-title"><span className="access-text">Studio Némésis</span></h1>
     	</section>
     );
   }
