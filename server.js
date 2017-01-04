9c88e4dd6894183afa56ad5f669eb113ad22e9cd
kimi <kimhimitsu@gmail.com>
2017-01-04 02:59:55 +0100

    move datas and medias to dist + update webpack config

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const path = require('path')
 const connect = require('connect')
 const serveStatic = require('serve-static')
 const PORT = process.env.PORT || 8080
 
-connect().use(serveStatic(__dirname)).listen(PORT, function(){
+connect().use(serveStatic(path.resolve(__dirname, 'dist/'))).listen(PORT, function(){
    console.log('Server running on ' + PORT + '...')
 })
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ let config = {
   target: 'web',
   entry: [
     './src/skeleton/' + PAGE,
-    './src/renderers/' + PAGE
+    './src/renderers/' + PAGE,
   ],
   output: {
     filename: PAGE + '.bundle.js',
@@ -22,11 +22,29 @@ let config = {
     loaders: [
       { 
         test: /\.jsx?$/, 
-        loader: 'babel-loader', 
+        loader: 'babel-loader',
+        exclude: /node_modules/,
         query: { 
           presets: ['es2015', 'stage-0'], 
           plugins: [ ['transform-react-jsx', { 'pragma':'h' }] ] 
-        } 
+        }
+      },
+        {
+        test: /\.(png|svg)$/i,
+        loader: 'url-loader',
+      },
+      {
+        test: /\.(gif|jpe?g)$/i,
+        loader: 'file-loader?name=[name].[ext]?[hash]',
+      },
+      {
+        test: /\.scss$/, 
+        loader: ExtractTextPlugin.extract('style-loader', [
+          'css-loader?-autoprefixer',
+          'resolve-url-loader',
+          'postcss-loader',
+          'sass-loader',
+        ])
       },
     ]
   }
@@ -35,42 +53,21 @@ let config = {
 if (process.env.NODE_ENV === 'development') {
 
   const PORT = process.env.PORT || 8080;
-
   config.entry.unshift('preact/devtools')
-  config.output.publicPath = `http://localhost:${PORT}/`
-  config.devtool = 'inline-source-map'
-  config.devServer =  {
+  config.devServer = {
     contentBase: './dist/',
     hot: true,
     port: PORT,
     inline: true,
   }
-
-  config.module.loaders.push({ 
-    test: /\.scss$/, 
-    loaders: [
-      'style-loader', 
-      'css-loader',
-      'resolve-url-loader',
-      'sass-loader',
-    ]
-  })
-
-  config.module.loaders.push({
-    test: /\.(png|svg|gif|jpe?g)$/i,
-    loader: 'file-loader?emitFile=false&name=[path][name].[ext]?[hash]',
-  })
-
   config.plugins = [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    }),
     new HtmlWebpackPlugin({
       filename: PAGE + '.html',
       template: './src/templates/' + PAGE,
       inject: true,
     }),
+    new ExtractTextPlugin('', {disable:true}),
   ] 
 
 }
@@ -79,30 +76,7 @@ else {
 
   config.entry.unshift('babel-polyfill') // ES6 Promises for all browsers <= to IE11, FF27, Safari7, Chrome32, Opera19, Old mobiles)
   config.entry.unshift('whatwg-fetch') // Fetch API for all browsers <= EDGE 13, IE11, Safari10, FF38, Chrome40, Opera26, Mobile browsers
-
-  config.output.publicPath = '/dist/'
-  config.devtool = 'cheap-source-map'
-
-  config.module.loaders.push({ 
-    test: /\.scss$/, 
-    loader: ExtractTextPlugin.extract([
-      'css-loader?-autoprefixer',
-      'resolve-url-loader',
-      'postcss-loader',
-      'sass-loader',
-    ])
-  })
-
-  config.module.loaders.push({
-    test: /\.(png|svg)$/i,
-    loader: 'url-loader',
-  })
-
-  config.module.loaders.push({
-    test: /\.(gif|jpe?g)$/i,
-    loader: 'file-loader?name=[name].[ext]?[sha512:hash:base64:7]',
-  })
-
+  config.devtool = 'cheap-module-source-map'
   config.plugins = [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
@@ -112,14 +86,14 @@ else {
       }
     }),
     new CompressionPlugin({
-        asset: "[path].gz[query]",
-        algorithm: "gzip",
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
         test: /\.js$/,
         threshold: 10240,
         minRatio: 0.8
     }),
     new HtmlWebpackPlugin({
-      filename: '../' + PAGE + '.html',
+      filename: PAGE + '.html',
       template: './src/templates/' + PAGE,
       inject: true,
       hash: true,
